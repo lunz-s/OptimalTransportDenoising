755caa9c54bf803581539e228fe5cee2c31d079c
SebLu1 <sl767@cam.ac.uk>
2018-04-06 15:35:23 +0100

    experiment added adv loss only

--- main.py
+++ main.py
@@ -65,4 +65,35 @@ if experiment == 2:
 
     at.train(300)
 
+if experiment == 3:
+
+    class pp_ad(postprocessing_adversarial):
+        experiment_name = 'High_transp_weight'
+
+        # learning rate for Adams
+        learning_rate = 0.001
+        # learning rate adversarial
+        learning_rate_adv = 0.0005
+        # weight adv net
+        trans_loss_weight = 1
+        # The batch size
+        batch_size = 32
+        # weight of soft relaxation regulariser adversarial net
+        lmb = 10
+        # default_adv_steps
+        def_adv_steps = 7
+        # default_gen_steps
+        def_gen_steps = 2
+
+        # noise level
+        noise_level = 0.1
+
+    at = pp_ad()
+
+    exp = input('Experiment type: ')
+    if exp == 1:
+        print(at.find_reg_parameter())
+
+    if exp == 2:
+        at.train(300)
 

03c1ec429934b19c77c76247de9c4ab7554c5ff3
SebLu1 <sl767@cam.ac.uk>
2018-04-11 15:07:47 +0100

    reduced learning rate

--- Framework.py
+++ Framework.py
@@ -309,9 +309,9 @@ class postprocessing_adversarial(generic_framework):
     model_name = 'PostProcessing'
 
     # learning rate for Adams
-    learning_rate = 0.0005
+    learning_rate = 0.0002
     # learning rate adversarial
-    learning_rate_adv = 0.0005
+    learning_rate_adv = 0.0002
     # weight adv net
     trans_loss_weight = 1
     # The batch size
@@ -319,7 +319,7 @@ class postprocessing_adversarial(generic_framework):
     # weight of soft relaxation regulariser adversarial net
     lmb= 10
     # default_adv_steps
-    def_adv_steps = 10
+    def_adv_steps = 12
     # default_gen_steps
     def_gen_steps = 1
 
